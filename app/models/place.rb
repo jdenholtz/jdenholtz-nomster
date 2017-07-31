ce548b34273074993a7ac37dad67a22872bfe738
Jacob Denholtz <jdenholtz@email.arizona.edu>
2017-07-31 12:03:12 +0000

    Ready to ship, could look a bit better

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,12 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photos
+
   
   geocoded_by :address
   after_validation :geocode
+
   
   validates :name, :address, :description, presence: true
   validates :name, length: { minimum: 4, too_short: "must be longer than 3 characters"}
